0779439b260c2d24a3be226414a4a76ccee0b538
root <root@res-mmb-web02.researchlumc.nl>
2026-05-28 17:38:46 +0200

    Initial commit

--- git_upload.sh
+++ git_upload.sh
@@ -7,5 +7,5 @@ git commit -m "Initial commit"
 
 git remote add origin https://github.com/IgorSidorovLUMC/LUMC_Tanzania_Virome-Analysis.git
 
-#git branch -M main
+git branch -M main
 #git push   -u origin main
